6bdd72a5e862f1237befd099e8ff80df5f750ef2
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2017-10-28 15:43:17 -0300

    - Include time param in vpu call

--- src/tests/fft_performance_tests.py
+++ src/tests/fft_performance_tests.py
@@ -5,7 +5,7 @@ from src.fft import fft
 
 #Process video, get frames and RGB channels analize an area of squareSize and then substract the mean
 #Params: videoName under path /Videos, a Location Area to analize and a squareSize
-[r,g,b,f] = vpu.getFilteredRGBVectors('2017-09-14 21.53.59.mp4', vpu.Location.CENTER, 30)
+[r,g,b,f] = vpu.getFilteredRGBVectors('fierens.mp4', vpu.Location.CENTER, 30,60)
 
 sta = time.perf_counter()
 G = np.abs(np.fft.fftshift(np.fft.fft(g))) ** 2

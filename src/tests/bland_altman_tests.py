6dec6789d3a952fb9cf606f14ecc70f6ccf6bf26
j1nma <j1nma@users.noreply.github.com>
2017-10-28 18:47:51 -0300

    B-A for blue channel.

--- src/tests/bland_altman_tests.py
+++ src/tests/bland_altman_tests.py
@@ -7,49 +7,43 @@ import cv2
 
 video_path = '../../res/videos/'
 video_format = '.mp4'
+subject_quantity = 4
 
-# without LED
+no_led_videos = vpu.get_no_led_videos()
+led_videos = vpu.get_led_videos()
 
-videos = vpu.get_no_led_videos()
+subject_videos = []
 
-no_led_freqs = []
+for n in no_led_videos:
+    for l in led_videos:
+        if n[2] == l[2]:
+            subject_videos.append([n[0], n[2]])
 
-for v in videos:
-
-    [r, g, b, f] = vpu.getFilteredRGBVectors(video_path + v[0] + '-' + v[1] + '-' + v[2] + video_format, vpu.Location.CENTER, 30, 61)
-    R = np.abs(np.fft.fftshift(np.fft.fft(r))) ** 2
-    G = np.abs(np.fft.fftshift(np.fft.fft(g))) ** 2
-    B = np.abs(np.fft.fftshift(np.fft.fft(b))) ** 2
-    no_led_freqs.append([abs(f[np.argmax(R)]) * 60, abs(f[np.argmax(G)]) * 60, abs(f[np.argmax(B)]) * 60])
-
-
-# with LED
-
-videos = vpu.get_led_videos()
-
-led_freqs = []
-
-for v in videos:
-
-    [r, g, b, f] = vpu.getFilteredRGBVectors(video_path + v[0] + '-' + v[1] + '-' + v[2] + video_format, vpu.Location.CENTER, 30, 61)
-    R = np.abs(np.fft.fftshift(np.fft.fft(r))) ** 2
-    G = np.abs(np.fft.fftshift(np.fft.fft(g))) ** 2
-    B = np.abs(np.fft.fftshift(np.fft.fft(b))) ** 2
-    led_freqs.append([abs(f[np.argmax(R)]) * 60, abs(f[np.argmax(G)]) * 60, abs(f[np.argmax(B)]) * 60])
-
-
-
-print(no_led_freqs)
 
 no_led_blues = []
-for f in no_led_freqs:
-    no_led_blues.append(f[2])
-
-
-print(no_led_blues)
-
 led_blues = []
-for f in no_led_freqs:
-    led_blues.append(f[2])
 
-# cmp.bland_altman(no_led_blues, led_blues)
+for s in subject_videos:
+
+    # without LED
+
+    [r, g, b, f] = vpu.getFilteredRGBVectors(video_path + s[0] + '-sinled-' + s[1] + video_format,
+                                             vpu.Location.CENTER, 30, 61)
+    # R = np.abs(np.fft.fftshift(fft.fft_iter_opt(r))) ** 2
+    # G = np.abs(np.fft.fftshift(fft.fft_iter_opt(g))) ** 2
+    B = np.abs(np.fft.fftshift(fft.fft_iter_opt(b))) ** 2
+    no_led_blues.append(abs(f[np.argmax(B)]) * 60)
+
+
+
+    # with LED
+
+    [r, g, b, f] = vpu.getFilteredRGBVectors(video_path + s[0] + '-led-' + s[1] + video_format, vpu.Location.CENTER, 30, 61)
+
+    # R = np.abs(np.fft.fftshift(fft.fft_iter_opt(r))) ** 2
+    # G = np.abs(np.fft.fftshift(fft.fft_iter_opt(g))) ** 2
+    B = np.abs(np.fft.fftshift(fft.fft_iter_opt(b))) ** 2
+    led_blues.append( abs(f[np.argmax(B)]) * 60)
+
+
+cmp.bland_altman(np.asarray(no_led_blues), np.array(led_blues))

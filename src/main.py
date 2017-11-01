56c6e9b0ea82d816dcb57aab2ff40627679502dd
AngieArlanti <angeles.arlanti@mercadolibre.com>
2017-10-31 21:36:49 -0300

    Cleaning code: examples deleted

--- src/main.py
+++
@@ -1,6 +0,0 @@
-from src.utils.video_processing_utils import getResourcesFromDirectory, getValidFileNames, get_led_videos, get_no_led_videos
-
-print(getResourcesFromDirectory())
-print(getValidFileNames())
-print(get_no_led_videos())
-print(get_led_videos())

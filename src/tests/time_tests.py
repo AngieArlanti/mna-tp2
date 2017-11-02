73720a7bc107b7448e436934f050b272a10bd778
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2017-11-01 23:15:58 -0300

    - Cleaned up time tests

--- src/tests/time_tests.py
+++ src/tests/time_tests.py
@@ -3,9 +3,7 @@ from src.utils import video_processing_utils as vpu
 from src.utils import plot_utils as pu
 from src.utils import fft_calc_utils as fcu
 import numpy as np
-from src.comparation_methods import r2
-
-
+from src.comparation_methods import get_coefficient_of_determination as r2
 
 
 video_path = '../../res/videos/'
@@ -13,31 +11,50 @@ video_format = '.mp4'
 videos = vpu.get_led_videos()
 led_freqs = []
 
-times = [10,15,20,25,30,60]
+times = [1,5,10,20,30,45,60]
+redHeartrates = []
+greenHeartrates = []
+blueHeartrates = []
+rgbHeartrates = []
+displayMatrix = []
 
+print("[Red channel R2 coefficient]"+'\t'+"[Green channel R2 coefficient]"+'\t'+"[Blue channel R2 coefficient]"+'\t'+"[Video length in seconds]")
 for t in times:
+    k = 0
     for v in videos:
 
-        [r, g, b, f] = vpu.getFilteredRGBVectors(video_path + v[0] + '-' + v[1] + '-' + v[2] + video_format, vpu.Location.CENTER, 30, t)
+        [r, g, b, f] = vpu.getFilteredRGBVectors(video_path + v[0] + '-' + v[1] + '-' + v[2] + video_format, vpu.Location.CENTER, 40, t)
         [R,G,B] = fcu.runFFTWithMethod(fcu.FFTMethod.FFT_ITER_OPT,r,g,b,f)
-
+        redHeartrates.append(fcu.getHeartRateFromBandVector(R,f))
+        greenHeartrates.append(fcu.getHeartRateFromBandVector(G, f))
+        blueHeartrates.append(fcu.getHeartRateFromBandVector(B, f))
         led_freqs.append([fcu.getHeartRateFromBandVector(R,f), fcu.getHeartRateFromBandVector(G,f), fcu.getHeartRateFromBandVector(B,f)])
 
+    fitbit = vpu.getFitbitHeartRates(vpu.LedPreference.LED)
+    rgbHeartrates.append([redHeartrates,greenHeartrates,blueHeartrates,fitbit])
+    pearsonR = r2(redHeartrates,fitbit)
+    pearsonG = r2(greenHeartrates,fitbit)
+    pearsonB = r2(blueHeartrates,fitbit)
+    print([str(pearsonR),str(pearsonG),str(pearsonB),str(t)])
+
+    redHeartrates = []
+    greenHeartrates = []
+    blueHeartrates = []
+    k = k+1
 
 fhr = vpu.getFitbitHeartRates(vpu.LedPreference.LED)
 k=0
 for x in led_freqs:
-    x.append(fhr[k%len(fhr)])
-    k=k+1
+        x.append(fhr[k%len(fhr)])
+k=k+1
 
-#print(vpu.getFitbitHeartRates(vpu.LedPreference.LED))
-#print(led_freqs)
 
+
+print("----------------------------------------------------------------------------------")
+print("Red"+'\t'+"Green"+'\t'+"Blue"+'\t'+"Fitbit measure")
 s = [[str(e) for e in row] for row in led_freqs]
 lens = [max(map(len, col)) for col in zip(*s)]
 fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
 table = [fmt.format(*row) for row in s]
 print('\n'.join(table))
-#print(vpu.getResourcesFromDirectory())
-
 
